2e34cff5b862fb15f918f28d282d13b2c4ef91ae
Wellington Pedro <wprsousa@gmail.com>
2025-07-27 10:58:39 -0300

    Add PessoaFisica and PessoaJuridica SQLAlchemy models

---
+++ src/models/sqlite/entities/pessoa_fisica.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, BIGINT, Float
+from src.models.sqlite.settings.base import Base
+
+
+class PessoaFisicaTable(Base):
+    __tablename__ = "pessoa_fisica"
+
+    id = Column(BIGINT, primary_key=True)
+    renda_mensal = Column(Float)
+    idade = Column(BIGINT)
+    nome_completo = Column(String)
+    celular = Column(String)
+    email = Column(String)
+    categoria = Column(String)
+    saldo = Column(Float)
+
+    def __repr__(self):
+        return (f"<PessoaFisica(id={self.id}, nome_completo={self.nome_completo}, celular={self.celular}, "
+                f"email={self.email}, categoria={self.categoria}, saldo={self.saldo})>")
